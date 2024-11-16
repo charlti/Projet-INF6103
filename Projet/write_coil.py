99ddbf56396e9681947dfda42b807020da95658e
charlti <charlti@Debian.myguest.virtualbox.org>
2024-11-16 22:54:01 +0100

    Changement script write_coil

--- Projet/write_coil.py
+++ Projet/write_coil.py
@@ -3,9 +3,25 @@ from pymodbus.client import ModbusTcpClient
 
 client = ModbusTcpClient('0.0.0.0', port=502)
 
-read_before = client.read_coils(0, 8)   # start reading from address 1
-print(read_before.bits)
-client.write_coil(0, False)
-time.sleep(5)
-reading_coil = client.read_coils(0, 8)   # start reading from address 1
-print(reading_coil.bits)
+# Établir la connexion avec le PLC
+if client.connect():
+    print("Connexion au PLC réussie.")
+    client.write_coil(0,True)
+    # Lire les valeurs des coils à partir d'une adresse donnée
+    adresse_debut = 0  # Adresse Modbus du premier coil (bobine)
+    nombre_coils = 9  # Nombre de coils à lire (lire 10 coils à partir de l'adresse 0)
+    
+    lecture = client.read_coils(adresse_debut, nombre_coils)
+    
+    if lecture.isError():
+        print("Erreur lors de la lecture des coils.")
+    else:
+        # Récupérer les valeurs des coils
+        valeurs_coils = lecture.bits
+        for i, valeur in enumerate(valeurs_coils, start=adresse_debut + 1):
+            print(f"Coil {i}: {'ON' if valeur else 'OFF'}")
+
+    # Déconnecter le client
+    client.close()
+else:
+    print("Impossible de se connecter au PLC.")
