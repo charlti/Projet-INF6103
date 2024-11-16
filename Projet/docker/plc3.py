09f5d8ba13b2cc0d795e320cbbef8cae40081d5e
charlti <charlti@Debian.myguest.virtualbox.org>
2024-11-16 02:41:12 +0100

    premier push partie indus

---
+++ Projet/docker/plc3.py
@@ -0,0 +1,27 @@
+from pymodbus.server import StartTcpServer
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+from pymodbus.datastore import ModbusSequentialDataBlock
+
+# Configuration des registres Modbus (ici Holding Registers)
+store = ModbusSlaveContext(
+    co=ModbusSequentialDataBlock(0, [True] * 10)  # 10 registres, initialisés à 0
+)
+context = ModbusServerContext(slaves=store, single=True)
+
+# Identification du dispositif simulé
+identity = ModbusDeviceIdentification()
+identity.VendorName = 'Python-PLC'
+identity.ProductCode = 'PLC003'
+identity.VendorUrl = 'https://example.com'
+identity.ProductName = 'Python Modbus PLC'
+identity.ModelName = 'PLC Simulator'
+identity.MajorMinorRevision = '1.0'
+
+# Fonction principale pour lancer le serveur
+def run_server():
+    print("Starting Modbus server...")
+    StartTcpServer(context=context, identity=identity, address=("192.168.1.13", 502))
+
+if __name__ == "__main__":
+    run_server()
